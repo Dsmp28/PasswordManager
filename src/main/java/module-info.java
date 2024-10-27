d15cf243a1aab1c5dbcb7b9e578a7ae0b3a31201
DavidM <dsmonje@correo.url.edu.gt>
2024-10-27 00:14:28 -0600

    Created the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.java.passwordmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.java.passwordmanager to javafx.fxml;
+    exports org.java.passwordmanager;
+}

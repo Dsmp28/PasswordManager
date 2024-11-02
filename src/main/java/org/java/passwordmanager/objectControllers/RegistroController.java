0d609e23e2c2806e262c09068c0a8904ccbfa2ac
Smorareq1 <smorareq1@gmail.com>
2024-11-02 15:01:05 -0600

    Busquedas

--- src/main/java/org/java/passwordmanager/objectControllers/RegistroController.java
+++ src/main/java/org/java/passwordmanager/objectControllers/RegistroController.java
@@ -6,9 +6,11 @@ import org.java.passwordmanager.objects.Registro;
 import org.java.passwordmanager.objects.Tag;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.time.LocalDate;
 
 public class RegistroController {
 
@@ -67,6 +69,115 @@ public class RegistroController {
     public static int getSize() {
         return registros.size();
     }
+
+    ////////////////////////////////////////// BUSQUEDAS ///////////////////////////////////////////////
+
+    public static List<Registro> searchBySiteName(String siteName) {
+        List<Registro> registrosEncontrados = new ArrayList<>();
+
+        for (Map.Entry<Integer, Registro> entry : registros.entrySet()) {
+            if (entry.getValue().getSiteName().equals(siteName)) {
+                registrosEncontrados.add(entry.getValue());
+            }
+        }
+        return registrosEncontrados;
+    }
+
+    public static List<Registro> searchByUsername(String username) {
+        List<Registro> registrosEncontrados = new ArrayList<>();
+
+        for (Map.Entry<Integer, Registro> entry : registros.entrySet()) {
+            if (entry.getValue().getUsername().equals(username)) {
+                registrosEncontrados.add(entry.getValue());
+            }
+        }
+        return registrosEncontrados;
+    }
+
+    public static List<Registro> searchByURL(String url) {
+        List<Registro> registrosEncontrados = new ArrayList<>();
+
+        for (Map.Entry<Integer, Registro> entry : registros.entrySet()) {
+            if (entry.getValue().getUrl().equals(url)) {
+                registrosEncontrados.add(entry.getValue());
+            }
+        }
+        return registrosEncontrados;
+    }
+
+    public static List<Registro> searchByNotes(String notes) {
+        List<Registro> registrosEncontrados = new ArrayList<>();
+
+        for (Map.Entry<Integer, Registro> entry : registros.entrySet()) {
+            if (entry.getValue().getNotes().equals(notes)) {
+                registrosEncontrados.add(entry.getValue());
+            }
+        }
+        return registrosEncontrados;
+    }
+
+    public static List<Registro> searchByCreationDate(LocalDate creationDate) {
+         List<Registro> registrosEncontrados = new ArrayList<>();
+
+        for (Map.Entry<Integer, Registro> entry : registros.entrySet()) {
+            if (entry.getValue().getCreationDate().toLocalDate().equals(creationDate)) {
+                registrosEncontrados.add(entry.getValue());
+            }
+        }
+
+        return registrosEncontrados;
+    }
+
+    public static List<Registro> searchByUpdateDate(LocalDate updateDate) {
+         List<Registro> registrosEncontrados = new ArrayList<>();
+
+        for (Map.Entry<Integer, Registro> entry : registros.entrySet()) {
+            if (entry.getValue().getUpdateDate().toLocalDate().equals(updateDate)) {
+                registrosEncontrados.add(entry.getValue());
+            }
+        }
+
+        return registrosEncontrados;
+    }
+
+    public static List<Registro> searchByExpirationDate(LocalDate expirationDate) {
+         List<Registro> registrosEncontrados = new ArrayList<>();
+
+        for (Map.Entry<Integer, Registro> entry : registros.entrySet()) {
+            if (entry.getValue().getExpirationDate().toLocalDate().equals(expirationDate)) {
+                registrosEncontrados.add(entry.getValue());
+            }
+        }
+
+        return registrosEncontrados;
+    }
+
+    public static List<Registro> searchByExtraField(String campoExtra) {
+        List<Registro> registrosEncontrados = new ArrayList<>();
+
+        for (Map.Entry<Integer, Registro> entry : registros.entrySet()) {
+            CamposExtra camposExtra = entry.getValue().getCamposExtra();
+            List<String> extras = camposExtra.getExtras();
+            for (String extra : extras) {
+                if (extra.equals(campoExtra)) {
+                    registrosEncontrados.add(entry.getValue());
+                    break;
+                }
+            }
+        }
+        return registrosEncontrados;
+    }
+
+
+
+    public static void mostrarRegistrosEncontrados(List<Registro> registrosEncontrados) {
+        for (Registro registro : registrosEncontrados) {
+            System.out.println(registro);
+        }
+        System.out.println("\n");
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
 
     //Prueba
     public static void mostrarRegistros() {
